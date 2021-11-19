06411ca0d2613d717840c1d8c7ed69f7be009e51
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-20 02:13:21 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,28 @@
+// 8.Loops
+// a.for loop
+// let arr=[1,2,3,4,5,6,7];
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+
+// b.foreach 
+// arr.forEach(function(element){
+//   console.log(element);
+// })
+
+// c.while loop
+// let arr=[1,2,3,4,5,6,7];
+// let i=0
+// while (i <arr.length ) {
+//     console.log(arr[i]);
+//     i++;
+//   }
+
+// d.do while loop
+// let arr=[1,2,3,4,5,6,7];
+// let i=0
+// do{
+//     console.log(arr[i]);
+//     i++;
+//   }while (i <arr.length );
