56dd553045b94b8d9dd881ed6aea7e779b4b6eca
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-20 02:11:53 +0530

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,30 @@
+// 7.conditionals in javascript
+// a.if statement 
+// var age=28;
+// if(age<=28)
+// {
+//     console.log("I am THOR");
+// }
+
+// b.if-else statement
+// var age =18;
+// if(age>=28){
+//     console.log("I am SPIDERMAN")
+// }
+// else{
+//     console.log("I am THOR");
+// }
+
+// c. if-else Ladder
+
+// var age =28;
+// if(age>27){
+//     console.log("I am groute")
+// }
+// else if(age<28)
+// {
+//     console.log("I am foxy");
+// }
+// else{
+//     console.log("I am ironman");
+// }
