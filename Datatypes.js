a9b63b146a8f3d9c8bf79a68dcdf95a87c6c5dd1
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-19 08:51:47 +0530

    Create Datatypes.js

---
+++ Datatypes.js
@@ -0,0 +1,38 @@
+/*
+4. There are two type of Data types in javascript
+1.Primitive Data type : number, string, undefined, null, boolean, symbol.
+2.Reference Data type : Arrays and objects.
+*/
+// a. number
+// var num1=25; 
+// console.log(num1);
+
+// b.string
+// var str1="This is divyanshu";
+// var str2='He is a developer';
+// console.log(str1);
+
+// c. Undefined --> (undefined means that --> In this variable have nothing.)
+// var und;
+// var und = undefined;
+// console.log(und);
+
+//d.Null-->  (Null means that --> In this variable have something, but it is null.)
+// var n = null;
+// console.log(n);
+
+//e. Booleans
+// var a=true;
+// var b=false;
+// console.log(a);
+
+// f.Arrays
+// var arr=["Divyanshu",1,2,3,"Sharma",7,9];
+// console.log(arr[0]);
+
+// Objects
+//  var marks={
+//      div:38,
+//      nishu:37,
+//      vashu:39}
+// console.log(marks);
