7fafa0452ca05c4d979a2c71259ffb225f5aa742
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-19 08:53:15 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,26 @@
+// 5.Operators
+// a.Arithmetics operators
+// var a=10;
+// var b=10;
+// console.log("The value of a+b =",a+b);
+// console.log("The value of a-b =",a-b);
+// console.log("The value of a*b =",a*b);
+// console.log("The value of a/b =",a/b);
+
+//b. Assignment operators
+// var g=10;
+// var c=g;
+// c+=3;
+// c*=3;
+// c/=3;
+// c-=3;
+// console.log(c);
+
+//c. Camparison operator
+// var x=56;
+// var y=65;
+// console.log(x==y);
+// console.log(x<=y);
+// console.log(x>=y);
+// console.log(x<y);
+// console.log(x>y);
