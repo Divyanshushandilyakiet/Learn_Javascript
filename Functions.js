c9cc77b947ee3e9e13e56efaeb580d7316a2b9c5
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-20 02:09:59 +0530

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,11 @@
+// 6.Functions in javascript
+// function avg(a,b){
+// c=(a+b)/2;
+// return c;
+// }
+
+// DRY--> do not repeat yourself
+// c1=avg(2,3);
+// c2=avg(8,9);
+// console.log(c1);
+// console.log(c2);
