46d8118b0a5f57bdf6326f2590c78bcd7cdae90b
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-20 02:45:06 +0530

    Create Date.js

---
+++ Date.js
@@ -0,0 +1,8 @@
+// 11. how to set the date 
+// let myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.getTime());
+// console.log(myDate.getMonth());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getHours());
+// console.log(myDate.getTimezoneOffset());
