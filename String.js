7418236ce16a95ffd3440a2215cdaf86810ff463
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-20 02:26:49 +0530

    Create String.js

---
+++ String.js
@@ -0,0 +1,6 @@
+// 10. String methods 
+// let myStr="Divyanshu is a good boy";
+// console.log(myStr.length);
+// console.log(myStr.indexOf("good"));
+// console.log(myStr.slice(15,19));
+// console.log(myStr.replace("Divyanshu","div"));
