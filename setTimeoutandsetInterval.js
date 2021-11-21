e1ebc6c08c88e59f8f052cc0d03fff6a0a4fd0f6
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-21 12:37:51 +0530

    Create setTimeoutandsetInterval.js

---
+++ setTimeoutandsetInterval.js
@@ -0,0 +1,22 @@
+// 14.setTimeout and setIntervals and arrow function
+// a.Arrow function
+// function add(a,b){
+//     return a+b;
+// }
+
+// Both are same
+ 
+// add=(a,b)=>{
+//     return a+b;
+// }
+
+// setTimeout
+// log=()=>{
+//  console.log('I am THOR')
+//  document.querySelectorAll('.container')[1].innerHTML="<b>ITS FIRE"
+// }
+// setTimeout(log,5000);
+// clearTimeout(clr);
+// clr=setInterval(log,5000);
+// clearInterval(clr);
+// use clearInterval and clearTimeout to cancel the interval and timeout
