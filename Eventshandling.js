5300091c60070c60689609cb686608ec2da3c770
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-21 12:37:07 +0530

    Create Eventshandling.js

---
+++ Eventshandling.js
@@ -0,0 +1,26 @@
+//13.Events in javascript
+// function clicked(){
+//     document.querySelectorAll('.container')[1].innerHTML="<b> We have clicked"
+//     console.log("The button is clicked");
+// }
+// window.onload=function(){
+//     console.log("The document was loaded");
+// }
+// Firstcontainer.addEventListener('click', function(){
+//     console.log("clicked on contianer");
+// })
+// Firstcontainer.addEventListener('mouseover', function(){
+//     console.log("Mouse on contianer");
+// })
+// Firstcontainer.addEventListener('mouseout', function(){
+//     console.log("Mouse out of the contianer");
+// })
+// let preHTML=document.querySelectorAll('.container')[1].innerHTML;
+// Firstcontainer.addEventListener('mouseup', function(){   
+//     document.querySelectorAll('.container')[1].innerHTML=preHTML;
+//     console.log("Mouse up when clicked on contianer");
+// })
+// Firstcontainer.addEventListener('mousedown', function(){
+//     document.querySelectorAll('.container')[1].innerHTML="<b> We have clicked"
+//     console.log("Mouse down when clicked on contianer");
+// })
