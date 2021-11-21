f6d51c3cd8cdacefe8fc99ab5cc4a5793e0bbf46
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-21 12:36:32 +0530

    Create DOMmanipulation.js

---
+++ DOMmanipulation.js
@@ -0,0 +1,26 @@
+// 12.DOM manipulation
+// a.
+// let elem=document.getElementById('click');
+// console.log(elem);
+// b.
+// let elemClass = document.getElementsByClassName('container')
+// console.log(elemClass);
+// elemClass[0].style.background="yellow";
+// elemClass[0].classList.add("bg-primary");
+// elemClass[0].classList.add("text-sucess");
+// console.log(elem.innerHTML);
+// console.log(elem.innerText);
+// console.log(elemClass[0].innerHTML);
+// console.log(elemClass[0].innerText);
+// c.
+//  tn= document.getElementsByTagName('div');
+//  console.log(tn);
+//  createdElement=document.createElement('p');
+//  createdElement.innerText= "This is a new paragraph";
+//  tn[0].appendChild(createdElement);
+
+// d. Selcting using query
+// sel=document.querySelector('.container');
+// console.log(sel);
+// sel=document.querySelectorAll('.container');
+// console.log(sel);
